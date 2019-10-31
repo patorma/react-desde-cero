c87b19b4cf8157dd5ff3536d7dff7ad6533f9147
patorma <pocontreras@ing.ucsc.cl>
2019-10-31 20:31:28 -0300

    ejemplo de uso react con jsx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 //import App from './App';
 
 const root = document.getElementById("root");
 //React.createElement(componente,propiedades,hijos)
-const elemento = React.createElement("h1", { className: "saludo" }, "Hola Mundo")
-    //caso en que no usaramos jsx
-    //dos parametros tiene render, el elemento a colocar y luego lugar del dom para ponerlo
-ReactDOM.render(elemento, root);
+/* const elemento = React.createElement("h1", { className: "saludo" }, "Hola Mundo") */
+//caso en que no usaramos jsx
+//dos parametros tiene render, el elemento a colocar y luego lugar del dom para ponerlo
+ReactDOM.render(<h1 className="saludo"> Hola Mundo </h1>, root);
