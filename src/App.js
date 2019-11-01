8608a45b602ea39392796d34377382f6ff7f967e
patorma <pocontreras@ing.ucsc.cl>
2019-10-31 22:14:41 -0300

    incluir varios elementos padres en uno con <Fragment>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./styles/style.scss";
 
 // caso que jsx no se use
 const App = () => (
     //Aqui se importa un solo elemento (el div) pero dentro de el pueden ir varios
-    <div>
-        <h1>Hola Mundo</h1>
-        <p>Hola, saludando desde mi componente</p>
-    </div>
+    <Fragment>
+        <div>
+            <h1>Hola Mundo</h1>
+            <p>Hola, saludando desde mi componente</p>
+        </div>
+        <div>
+            <h2>Hola Mundo soy un h2</h2>
+            <p>Hola, saludando desde mi componente, yo soy un h2</p>
+        </div>
+    </Fragment>
 );
 
 export default App;
