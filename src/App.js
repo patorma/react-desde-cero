2304afd81306a8b24126b06cdddd3ed3e59ca22e
patorma <pocontreras@ing.ucsc.cl>
2019-10-31 22:22:03 -0300

    Incluir varios elementos padres con <></> tambien

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./styles/style.scss";
 
 // caso que jsx no se use
 const App = () => (
     //Aqui se importa un solo elemento (el div) pero dentro de el pueden ir varios
-    <Fragment>
+    //<Fragment></Fragment> primeraforma
+    <>
         <div>
             <h1>Hola Mundo</h1>
             <p>Hola, saludando desde mi componente</p>
@@ -13,7 +14,7 @@ const App = () => (
             <h2>Hola Mundo soy un h2</h2>
             <p>Hola, saludando desde mi componente, yo soy un h2</p>
         </div>
-    </Fragment>
+    </>
 );
 
 export default App;
@@ -21,3 +22,4 @@ export default App;
 //Reglas JSX
 //1: Toda etiqueta debe cerrarse
 //2 : Los componentes deben devolver un solo elemento padre
+//3: Apoyarse de los Fragment cuando necesito devolver 2 elementos
